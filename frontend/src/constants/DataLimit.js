3b35d7bc6299c67e15dcd00783ffe8b9196b9c3e
Dimi Mikadze <dimi@udilia.com>
2019-10-01 16:18:43 +0400

    Increase data limit constants

--- frontend/src/constants/DataLimit.js
+++ frontend/src/constants/DataLimit.js
@@ -2,12 +2,12 @@
  * Data limit constants for infinite scroll
  */
 
-export const HOME_PAGE_POSTS_LIMIT = 3;
+export const HOME_PAGE_POSTS_LIMIT = 6;
 
-export const EXPLORE_PAGE_POSTS_LIMIT = 9;
+export const EXPLORE_PAGE_POSTS_LIMIT = 15;
 
-export const PEOPLE_PAGE_USERS_LIMIT = 12;
+export const PEOPLE_PAGE_USERS_LIMIT = 15;
 
-export const NOTIFICATIONS_PAGE_NOTIFICATION_LIMIT = 12;
+export const NOTIFICATIONS_PAGE_NOTIFICATION_LIMIT = 30;
 
-export const PROFILE_PAGE_POSTS_LIMIT = 3;
+export const PROFILE_PAGE_POSTS_LIMIT = 6;
