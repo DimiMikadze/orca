bf21311cf323bbb172e05fa1694a67c41d703de7
DimiMikadze <dimimikadze@gmail.com>
2021-10-09 22:05:16 +0400

    Update google analytics tracking ID

--- packages/orca-frontend/utils/config.ts
+++ packages/orca-frontend/utils/config.ts
@@ -3,5 +3,5 @@ const API_DEV_URL = 'http://localhost:4000';
 
 export const Config = {
   API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
-  GOOGLE_ANALYTICS_ID: 'G-S35Z7Y1YR1',
+  GOOGLE_ANALYTICS_ID: 'G-MD706N8TZV',
 };
