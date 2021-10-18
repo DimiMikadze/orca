21d30594dcb8a0d7e8ae792cdbce14272f1b9209
Tom Kochievi <t.kochievi@gmail.com>
2021-10-18 01:40:26 -0700

    New icons for the left sidebar (#169)
    * New icons for the sidebar

--- packages/orca-frontend/components/ui/icons/HouseColorfulIcon.tsx
+++ packages/orca-frontend/components/ui/icons/HouseColorfulIcon.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import theme from '../../../theme';
 
 interface HouseColorfulIconProps {
   width?: string;
@@ -10,29 +9,21 @@ const HouseColorfulIcon: FC<HouseColorfulIconProps> = ({ width }) => {
   const DEFAULT_WIDTH = '22';
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={width || DEFAULT_WIDTH}>
-      <path fill={theme.colors.general.error} d="M382.933 73.6H441.6V176h-58.667z" />
-      <path fill={theme.colors.general.error} d="M360.533 21.333H464V76.8H360.533z" />
-      <path
-        d="M44.8 276.267V512h418.133V276.267L253.866 67.2C172.8 149.333 126.933 195.2 44.8 276.267z"
-        fill={theme.colors.general.warning}
-      />
-      <path
-        d="M464 276.267L254.933 67.2c-41.6 40.533-73.6 72.533-105.6 104.533L44.8 276.266v48l209.067-209.067 209.067 209.067v6.4-54.4H464z"
-        fill={theme.colors.general.warning}
-      />
-      <path
-        d="M216.533 512H90.667V388.267c0-35.2 28.8-62.933 62.933-62.933 35.2 0 62.933 28.8 62.933 62.933V512z"
-        fill={theme.colors.general.warning}
-      />
-      <path fill={theme.colors.grey[60]} d="M253.867 325.333h164.267v144H253.867z" />
-      <g fill={theme.colors.grey[30]}>
-        <path d="M270.933 342.4h56.533v109.867h-56.533zM344.533 342.4h56.533v109.867h-56.533z" />
+    <svg width={width || DEFAULT_WIDTH} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.5 32.65">
+      <g data-name="Layer 2">
+        <g data-name="Layer 1">
+          <path
+            data-name="Path 67"
+            d="M19.34 27.76v-3.54a1.68 1.68 0 0 0-1.51-1.84h-3.22a1.54 1.54 0 0 0-1.55 1.52c0 2.18 0 4.36-.06 6.54v1.24a.87.87 0 0 1-.82.93h-.06c-1.81 0-3.62.07-5.43 0a3.45 3.45 0 0 1-3.26-3.53V19.8c0-.49 0-.48-.49-.5a2.88 2.88 0 0 1-2.66-1.76 3 3 0 0 1 .61-3.36l3.25-3.27q5-5 9.89-9.94a3 3 0 0 1 2.87-.9 2.72 2.72 0 0 1 1.4.76q6.7 6.69 13.32 13.4a2.79 2.79 0 0 1 .61 3.24 2.82 2.82 0 0 1-2.71 1.84c-.53 0-.53 0-.53.55v9.41a3.49 3.49 0 0 1-3.36 3.36h-5.22a1 1 0 0 1-1.06-.89v-.18c-.01-1.27-.01-2.56-.01-3.8Z"
+            fill="#00a699"
+          />
+          <path
+            data-name="Path 68"
+            d="M16.24 0A3 3 0 0 0 14 1q-4.95 5-9.89 9.94C3.05 12 2 13.08.88 14.18a3 3 0 0 0-.61 3.35A2.87 2.87 0 0 0 3 19.3c.49 0 .49 0 .49.49v9.28a3.45 3.45 0 0 0 3.26 3.53c1.8.08 3.61 0 5.42 0a.88.88 0 0 0 .88-.88v-1.3c0-2.18 0-4.35.06-6.53a1.54 1.54 0 0 1 1.54-1.54h1.59Z"
+            fill="#04d3c3"
+          />
+        </g>
       </g>
-      <path
-        d="M502.4 232.533L279.467 9.6c-12.8-12.8-35.2-12.8-48 0L9.6 232.533c-12.8 12.8-12.8 35.2 0 48 12.8 12.8 35.2 12.8 48 0L256 82.133l198.4 198.4c12.8 12.8 35.2 12.8 48 0 12.8-13.866 12.8-35.2 0-48z"
-        fill={theme.colors.general.error}
-      />
     </svg>
   );
 };
